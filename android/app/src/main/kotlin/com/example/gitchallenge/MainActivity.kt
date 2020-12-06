63cc9fd80b562fecb160837b7208fdb755d3a316
thirihlaing <thirih@gmail.com>
2020-12-06 21:03:53 +0630

    version 2

---
+++ android/app/src/main/kotlin/com/example/gitchallenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gitchallenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar
+                (
+                backgroundColor: Colors.green,
+                radius: 50.0,
+                backgroundImage: AssetImage('images/cute.jpg')
+
+              ),
+              Text(
+                'thirihlaing',style: TextStyle(
+                fontFamily: 'Sacramento',
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+
+              ),
+              ),
+              Text(
+                'web delover',style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                letterSpacing: 2.5,
+              ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 100,
+                child: Divider(
+                  color: Colors.blue.shade100,
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  // padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+959264886229',style: TextStyle(
+                        color: Colors.teal
+                    ),
+                    ),
+
+                  )
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 0,horizontal: 20),
+                //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,color: Colors.teal,
+                  ),
+                  title: Text(
+                    'thirih@gmail.com',style: TextStyle(
+                      color: Colors.teal
+                  ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
